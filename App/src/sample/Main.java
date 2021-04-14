aacdd257a676a4b43782071c7f1f61db457f08fd
xslimShadyx <husseinbja@yahoo.de>
2021-04-14 20:00:13 +0200

    first face of location algorithm

--- App/src/sample/Main.java
+++ App/src/sample/Main.java
@@ -7,21 +7,58 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sample.model.RssAlgorithm;
 
-public class Main extends Application {
+import javax.swing.*;
+import java.awt.*;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+public class Main extends Canvas {
 
-    }
+    Point point1 = new Point(150,0);
+    Point point2 = new Point(150,280);
+    Point point3 = new Point(280,150);
+    RssAlgorithm rssAlgorithm = new RssAlgorithm(2417,point1,point2,point3);
+
 
 
     public static void main(String[] args) {
-        RssAlgorithm rssAlgorithm = new RssAlgorithm(50,2417);
-        rssAlgorithm.distanceBetweenNodeAndSource();
-        launch(args);
+
+        JFrame frame = new JFrame("Room");
+        Canvas canvas = new Main();
+        canvas.setSize(300,300);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+
     }
+    public void paint(Graphics g) {
+        rssAlgorithm.setP1dbm(61);
+        rssAlgorithm.setP2dbm(41);
+        rssAlgorithm.setP3dbm(61);
+        int m=3;
+        int x  =(int) point1.getX();
+        int y=(int) point1.getY();
+        while (m>=0) {
+            if (m==2){
+                x =(int) point2.getX();
+                y=(int) point2.getY();
+
+            }
+            if (m==1){
+                x =(int) point3.getX();
+                y=(int) point3.getY();
+            } if (m==0){
+
+                x =(int) rssAlgorithm.targetPosition().getX();
+                y=(int) rssAlgorithm.targetPosition().getY();
+
+
+            }
+
+
+            g.fillOval(x, y, 15, 15);
+            m--;
+
+        }
+
+    }
+
 }
--- App/src/sample/model/RssAlgorithm.java
+++ App/src/sample/model/RssAlgorithm.java
@@ -1,31 +1,73 @@
 package sample.model;
+import java.awt.*;
 import java.lang.Math;
+import java.util.HashMap;
+import java.util.Map;
 
 
 // this class is responsible of signal strength algorithm calculation
 public class RssAlgorithm {
-   private int dbm ;
-    private int frequency;
 
- public RssAlgorithm(int dbm ,int frequency){
-    this.dbm=dbm;
+    private int frequency;
+    private  Point ab1;
+    private Point ab2 ;
+    private  Point ab3 ;
+    int p1dbm=0;
+    int p2dbm=0;
+    int p3dbm=0;
+
+ public RssAlgorithm(int frequency, Point ab1 ,Point ab2 ,Point ab3){
+
     this.frequency=frequency;
+    this.ab1=ab1;
+    this.ab2=ab2;
+    this.ab3=ab3;
+
 }
+
+    public void setP1dbm(int p1dbm) {
+        this.p1dbm = p1dbm;
+    }
+
+    public void setP2dbm(int p2dbm) {
+        this.p2dbm = p2dbm;
+    }
+
+    public void setP3dbm(int p3dbm) {
+        this.p3dbm = p3dbm;
+    }
 
     /**
      * This method is to give the predicted distance between a node and transmitter.
      * @FSPL is free space path loss
      */
-   public void distanceBetweenNodeAndSource(){
+   public int  distanceBetweenNodeAndSource( int dbm){
     double FSPL = 27.55;
 
-    double d = Math.pow(10 , (((FSPL-(20*Math.log10(frequency)))+Math.abs(dbm))/20));
-        d= Math.round(d);
+    double d =  Math.pow(10 , (((FSPL-(20*Math.log10(frequency)))+Math.abs(dbm))/20));
 
+        d=  Math.round(d);
 
-        System.out.println("The predicted distance is " +d+" m");
+        return (int) d;
 
     }
 
+
+
+   public Point targetPosition(){
+       int d1 = distanceBetweenNodeAndSource(p1dbm);
+       int d2 = distanceBetweenNodeAndSource(p2dbm);
+       int d3 = distanceBetweenNodeAndSource(p3dbm);
+
+       int y1 = (int) (ab1.getY()-(d1*10));
+       int y2 = (int) (ab2.getY()+(d2*10));
+       int y = y1+y2/2;
+       System.out.println("y"+y);
+       int x = (int) (ab3.getX()-(d3*10));
+       System.out.println("x"+x);
+       Point target = new Point();
+
+       return new Point(x,y);
+    }
 
 }
