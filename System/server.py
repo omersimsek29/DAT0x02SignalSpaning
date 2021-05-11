b77e71f0bf1cb11c73024de6ed0f0d9e03497057
Emil Holmberg <emilholmberg@live.se>
2021-05-11 03:30:12 +0200

    Update server.py
    Algoritm och system kopplat, har glömt koppla.

--- System/server.py
+++ System/server.py
@@ -2,7 +2,9 @@ import pickle
 import queue
 import socket
 import threading
+from math import log10
 import time
+import math
 
 ### GLOBALS
 
@@ -21,13 +23,108 @@ dataQueues =  [queue.Queue()] * max_conns
 connections = []
 discStr = 'disconnect'
 startStr = 'start'
+point1 = (6,5)
+point2 = (3, 2.5)
+point3 = (6,0)
+frequency = 2412
 
+### KLASSER
+
+class RSSAlgorithm:
+
+    frequency = 0 
+    point1 = (0,0)
+    point2 =(0,0)
+    point3 = (0,0)
+    
+    def __init__(self, frequency, point1, point2, point3):
+        self.frequency = frequency
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
+
+
+
+     
+def distance_between_node_and_source(dbm,frequency):
+    fspl = 27.55
+    dist = 10 ** ((fspl -20* log10(frequency)+abs(dbm))/20)
+    dist = round(dist,2)
+    return dist
+
+
+def targeted_positon(point1,point2,point3,dbm1,dbm2,dbm3,frequency):
+    d1 = distance_between_node_and_source(dbm1,frequency)
+    d2 = distance_between_node_and_source(dbm2,frequency)
+    d3 = distance_between_node_and_source(dbm3 ,frequency)
+    
+    if point1.y<point2.y:
+        y = point1.y+d1
+    elif point1.y > point2.y:
+        y = point2.y - d2
+        
+    else:
+      x = point3.x-d3
+            
+      return (x,y)
+  
+  
+  
+def targeted_positon2(point1,point2,point3,dbm1,dbm2,dbm3,frequency):
+      
+    d1 = distance_between_node_and_source(dbm1,frequency)
+    d2 = distance_between_node_and_source(dbm2,frequency)
+    d3 = distance_between_node_and_source(dbm3 ,frequency)
+    
+    a = (-2 * point1[0]) + (2*point2[0])
+    b = (-2 * point1[1]) + (2*point2[1])
+    d = (-2 * point2[0]) + (2*point3[0])
+    e = (-2 * point2[1]) + (2*point3[1])
+    
+    c = (d1**2) - (d2 **2) - (point1[0]**2)+(point2[0]**2)-(point1[1]**2)+(point2[1]**2)
+    f = (d2**2) - (d3 **2) - (point2[0]**2)+(point3[0]**2)-(point2[1]**2)+(point3[1]**2)
+    
+    x = ((c*e)-(f*b))/((e*a)-(b*d))
+    y = ((c*d)-(a*f))/((b*d)-(a*e))
+    
+    return (x,y)
+
+#rssAlg = RSSAlgorithm(2412, point1, point2, point3)
 ### FUNKTIONER
+
+def distance_between_node_and_source(dbm,frequency):
+    fspl = 27.55
+    dist = 10 ** ((fspl -20* log10(frequency)+abs(dbm))/20)
+    dist = round(dist,2)
+    return dist
+
+def targeted_positon2(point1,point2,point3,dbm1,dbm2,dbm3,frequency):
+      
+    d1 = distance_between_node_and_source(dbm1,frequency)
+    d2 = distance_between_node_and_source(dbm2,frequency)
+    d3 = distance_between_node_and_source(dbm3 ,frequency)
+    
+    a = (-2 * point1[0]) + (2*point2[0])
+    b = (-2 * point1[1]) + (2*point2[1])
+    d = (-2 * point2[0]) + (2*point3[0])
+    e = (-2 * point2[1]) + (2*point3[1])
+    
+    c = (d1**2) - (d2 **2) - (point1[0]**2)+(point2[0]**2)-(point1[1]**2)+(point2[1]**2)
+    f = (d2**2) - (d3 **2) - (point2[0]**2)+(point3[0]**2)-(point2[1]**2)+(point3[1]**2)
+    
+    x = ((c*e)-(f*b))/((e*a)-(b*d))
+    y = ((c*d)-(a*f))/((b*d)-(a*e))
+    
+    return (x,y)
 
 # funktion där position beräknas när algorithm tråden fått data från alla ankare. 
 def algorithm(extracted_datas):
     print('calculate position')
-
+def inExamRoom(point): #TODO
+    if point == False:
+        return ' inte belägen i tentasal'
+    else:
+        return 'belägen i tentasal'
 # tänkt som en separat tråd som synkar algoritm beräkningar när servern mottagit data från alla
 # ankare
 def algorithm_thread():
@@ -41,21 +138,31 @@ def algorithm_thread():
             extracted_datas = []
             for index in range(len(dataQueues)):
                 extracted_datas.append(dataQueues[index].get())
-            print('took data from both threads at ' + str(time.time()))
-            print('queue 1: ')
-            print(extracted_datas[0])
-            print('queue 2: ')
-            print(extracted_datas[1])
-            print('--------------------------------------')
+            #print('took data from both threads at ' + str(time.time()))
+            #print('queue 1: ')
+            #print(extracted_datas[0])
+            #print('queue 2: ')
+            #print(extracted_datas[1])
+            #print('--------------------------------------')
             # kommentera ut antingen de printsen ovanför eller for looparna under för att enklare läsa vad som sker, dvs ha bara antingen eller aktiv
             for index in range(extracted_datas[0].shape[0]): # källan måste registrerats hos alla ankare för att vara relevant.
                 shared_rows = []
                 for row in extracted_datas:
                     shared_rows.append(row.loc[row['source'] == extracted_datas[0].iloc[index]['source']])
                 if len(shared_rows) == max_conns:
+                    calculated_pos = (0,0)
                     for i in range(len(shared_rows)):
-                        print('Signal strength of client : ' + str(i) + ' ' + str(shared_rows[i]['signal strength']))
+                        if len(shared_rows[0]['signal strength'].values) > 0 and  len(shared_rows[1]['signal strength'].values) > 0:
+                            calculated_pos = targeted_positon2(point1, point2, point3, shared_rows[0]['signal strength'].values[0], shared_rows[1]['signal strength'].values[0], -60, frequency) # ska vara en till med index 2 när alla ankare är anslutna
+                        #print(shared_rows)
+                        #print(shared_rows[0]['signal strength'].values[0])
+                        #print('\n')
+                        #print(shared_rows[1]['signal strength'])
+                        #print(calculated_pos[0])
+                        print(str(shared_rows[0]['source'].values[0]) + ' är beräknad att ligga på ' + str(calculated_pos) + ' och ' + str(inExamRoom(False)))
+                        #print('Signal strength of client : ' + str(i) + ' ' + str(shared_rows[i]['signal strength']))
                 #shared_rows skickas sedan till algoritmen
+            
 # returnerar index för given ip-adress
 def indexFromIp(ip):
     return ipAdresses.index(ip)
